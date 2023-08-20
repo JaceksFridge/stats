e0667e7b01b87e5f091c98e1e1c45dd6e758d412
JaceksFridge <Japacz22@gmail.com>
2023-08-20 22:17:11 +0200

    relative path

--- get_stats/exts.py
+++ get_stats/exts.py
@@ -3,15 +3,17 @@
 import os
 import json
 
-filename = "/path/to/your/file.txt"
-
-
 
 ext = ".json"
 
 def exts_dict():
+    # Find the directory where the current script (exts.py) is located
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # Construct the path to the languages.json file based on the directory of exts.py
+    languages_json_path = os.path.join(current_dir, "data", "languages.json")
     exts = {}
-    with open("./data/languages.json", "r") as file:
+    with open(languages_json_path, "r") as file:
         data = json.load(file)
         
         for language in data:
