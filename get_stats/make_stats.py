66e24fe9d28fb02967596b6c204a7fb738b45727
JaceksFridge <Japacz22@gmail.com>
2023-08-20 17:41:30 +0200

    command line

--- get_stats/make_stats.py
+++ get_stats/make_stats.py
@@ -1,7 +1,6 @@
 
 
-
-
+#!/usr/bin/env python3
 import os
 import csv
 from tabulate import tabulate
---
+++ setup.py
@@ -0,0 +1,14 @@
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name="getstats",
+    version="0.1",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'getstats = get_stats.make_stats:main',
+        ],
+    },
+)
