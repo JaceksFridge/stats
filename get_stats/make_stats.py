e37d51dca55ead4c101e4c1ba9ce10be1413855c
JaceksFridge <Japacz22@gmail.com>
2023-08-21 19:27:03 +0200

    added ascii chart

--- get_stats/make_stats.py
+++ get_stats/make_stats.py
@@ -6,8 +6,8 @@ import csv
 from tabulate import tabulate
 from get_stats.exts import exts_dict
 import chardet
-
-
+import pandas as pd
+import asciibars as a_bars
 
 exts_tresor = exts_dict()
 
@@ -45,9 +45,10 @@ def main():
             
     file_tresor = sorted(file_tresor, key=lambda x: x[1], reverse=True)
     
-    make_lang_table(file_tresor)
+    lang_df = make_lang_df(file_tresor)
+    lang_chart = make_lang_chart(lang_df)
     table = make_table(file_tresor)
-    stat_file = make_stat_file(path, table)
+    stat_file = make_stat_file(path, table, lang_chart)
     
     
     
@@ -80,16 +81,43 @@ def make_table(file_tresor):
     table = tabulate(file_tresor, headers, tablefmt="rst")
     return table
 
-def make_lang_table(file_tresor):
-    for file in file_tresor:
-        print(file)
+def make_lang_df(file_tresor):
+    df = pd.DataFrame(file_tresor, columns=["Filename", "Lines", "Language"])
+    lang_df = df.groupby('Language')['Lines'].sum().reset_index()
+    
+    total_lines = lang_df['Lines'].sum()
+    lang_df['Percentage'] = round((lang_df['Lines'] / total_lines) * 100, 2)
+    
+    lang_df = lang_df.sort_values(by='Percentage', ascending=False)
+    return lang_df
 
 
-def make_stat_file(path, table):
+def make_stat_file(path, table, lang_chart):
     stats_path = os.path.join(path, "stats.txt")
     with open(stats_path, "w") as stats_file:
+        stats_file.write("\n\n")
+        stats_file.write(lang_chart)  
+        stats_file.write("\n\n") 
         stats_file.write(table)
-            
+
+ 
+import io
+import sys
+
+def make_lang_chart(lang_df):
+    data = []
+    for _, row in lang_df.iterrows():
+        data.append((str(row['Language']), float(row['Percentage'])))
+          
+    old_stdout = sys.stdout
+    new_stdout = io.StringIO()
+    sys.stdout = new_stdout
+
+    a_bars.plot(data, unit='▓', neg_unit='░')
+    sys.stdout = old_stdout
+
+    return new_stdout.getvalue()
+    
     
 if __name__ == "__main__":
     main()
